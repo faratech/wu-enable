bdf6409b3cca91247452726e14a18c050124bd45
Mike Fara <mjfara@gmail.com>
2024-04-19 05:46:06 -0400

    Added argument

--- wu-enable.ps1
+++ wu-enable.ps1
@@ -1,95 +1,212 @@
-<#
-.SYNOPSIS
-This script performs various actions to disable the SCCM client, block SCCM servers in the HOSTS file, reset Group Policy Objects, modify Windows Update settings, and schedule the script to run periodically.
-
-.DESCRIPTION
-The script first checks if the SCCM client is installed and uninstalls it silently if found. It then modifies the HOSTS file to block SCCM servers by adding entries for loopback IP addresses. Next, it resets Group Policy Objects by removing relevant registry keys and folders. After that, it sets a registry key to enable Windows Update. Finally, it schedules the script to run every 15 minutes and on startup using the Task Scheduler.
-
-.PARAMETER None
-
-.EXAMPLE
-.\wu-enable.ps1
-Runs the script to perform the actions described above.
-
-.NOTES
-Author: Mike Fara
-Date: 2024.04.16
-Version: 1.0
-#>
-
-# Uninstall SCCM Client silently
-$SCCMClientUninstall = "C:\Windows\ccmsetup\ccmsetup.exe"
-if (Test-Path $SCCMClientUninstall) {
-    Invoke-Expression "$SCCMClientUninstall /uninstall /quiet /norestart"
-    Write-Host "SCCM Client has been uninstalled silently."
-} else {
-    Write-Host "SCCM Client is not installed or the path is incorrect."
-}
-
-# Modify HOSTS file to block SCCM Servers
-$hostsPath = "$env:windir\System32\drivers\etc\hosts"
-$hostsContent = Get-Content -Path $hostsPath
-$hostsEntries = @(
-    "127.0.0.1 wmcsccm02.mydomain.com",
-    "127.0.0.1 wmcsccm03.mydomain.com",
-    "127.0.0.1 wmcsccm02",
-    "127.0.0.2 wmcsccm03"
+param(
+    [Parameter(Mandatory=$false)]
+    [switch]$undo
 )
+$SCCMServers = @(
+    "wmcsccm02.wcmc.com",
+    "wmcsccm03.wcmc.com",
+    "wmcsccm02",
+    "wmcsccm03"
+)
+$ScriptRoot = "c:\temp\wu-enable"
+$ScriptName = Split-Path -Path $PSCommandPath -Leaf
+$ScriptPath = "$ScriptRoot\$ScriptName"
+function AdminCheck {
+    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        # Check if the script is running in Visual Studio Code
+        if ($host.Name -eq "Visual Studio Code Host") {
+            Write-Host "Please restart Visual Studio Code with administrative privileges to run this script."
+            exit
+        }
+        # The script is not running with administrative privileges, so relaunch it with administrative privileges
+        # Create a new process start info
+        $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+        # Set the file name to PowerShell
+        $startInfo.FileName = if ($PSVersionTable.PSVersion.Major -ge 7) { "pwsh" } else { "powershell" }
+        # Set the arguments to the script path
+        $startInfo.Arguments = "-Command {& '" + $script:MyInvocation.MyCommand.Path + "'}"
+        # Set the verb to run as
+        $startInfo.Verb = "runas"
+        # Start the new process
+        [System.Diagnostics.Process]::Start($startInfo)
+        # Exit the current script
+        exit
+    }
+}
+    function ManagePowerShellInstallation {
+        # Check if PowerShell 7 is installed
+        if (-not (Test-Path "C:\Program Files\PowerShell\7\pwsh.exe")) {
+            # PowerShell 7 is not installed, so install it
+            Write-Host "PowerShell 7 is not installed. Installing..."
+            # Get the latest release from the GitHub API
+            $latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/PowerShell/PowerShell/releases/latest"
+            # Get the download URL of the MSI installer for x64 systems
+            $downloadUrl = $latestRelease.assets | Where-Object { $_.name -like "*-win-x64.msi" } | Select-Object -ExpandProperty browser_download_url
+            # Download the installer
+            Invoke-WebRequest -Uri $downloadUrl -OutFile "$env:TEMP\PowerShell-7-win-x64.msi"
+            # Run the installer
+            Start-Process -FilePath "$env:TEMP\PowerShell-7-win-x64.msi" -Args "/quiet" -Wait
+            Write-Host "PowerShell 7 installed successfully."
+        } else {
+            # PowerShell 7 is installed, so check if it needs to be updated
+            # Get the version of PowerShell 7
+            $currentVersion = & "C:\Program Files\PowerShell\7\pwsh.exe" -Command { $PSVersionTable.PSVersion }
+            # Get the latest release from the GitHub API
+            $latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/PowerShell/PowerShell/releases/latest"
+            $latestVersion = [Version]$latestRelease.tag_name.Replace('v', '')
+            if ($currentVersion -lt $latestVersion) {
+                # PowerShell 7 is out of date, so update it
+                Write-Host "PowerShell 7 is out of date. Updating..."
+                # Get the download URL of the MSI installer for x64 systems
+                $downloadUrl = $latestRelease.assets | Where-Object { $_.name -like "*-win-x64.msi" } | Select-Object -ExpandProperty browser_download_url
+                # Create a job to download and install the update
+                $job = Start-Job -ScriptBlock {
+                    Invoke-WebRequest -Uri $using:downloadUrl -OutFile "$env:TEMP\PowerShell-7-win-x64.msi"
+                    Start-Process -FilePath "$env:TEMP\PowerShell-7-win-x64.msi" -Args "/quiet" -Wait
+                }
+                # Wait for the job to complete
+                $null = Receive-Job -Job $job -Wait
+                # Remove the job
+                Remove-Job -Job $job
+                Write-Host "PowerShell 7 updated successfully."
+            } else {
+                # PowerShell 7 is up to date
+                Write-Host "PowerShell 7 is up to date."
+            }
+        }
+    }
 
-foreach ($entry in $hostsEntries) {
-    if ($hostsContent -notcontains $entry) {
-        Add-Content -Path $hostsPath -Value $entry
+function ScheduleScript ($undo) {
+    # Schedule this script to run every 15 minutes and on startup
+    $TaskName = "RunWUEnableScript"
+    if (Test-Path "C:\Program Files\PowerShell\7\pwsh.exe") {
+        $powershellPath = "C:\Program Files\PowerShell\7\pwsh.exe"
+        $powerShellVersion = "7"
+        } else {
+        $powershellPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+        $powerShellVersion = "5.1"
+        }
+    if (-not $undo) {
+        # Check if the task already exists
+        if (-not (Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue)) {
+            $nextMinute = (Get-Date).AddMinutes(1)
+            $trigger = New-ScheduledTaskTrigger -Once -At $nextMinute -RepetitionInterval (New-TimeSpan -Minutes 15)            
+            $action = New-ScheduledTaskAction -Execute $powershellPath -Argument "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$ScriptPath`""
+            $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+            $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
+            $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings
+            Register-ScheduledTask -TaskName $TaskName -TaskPath "\" -InputObject $task
+
+            Write-Host "Scheduled task created successfully and will run every 15 minutes and on startup."
+        } else {
+            Write-Host "Scheduled task already exists. No action taken."
+        }
+    } else {
+        # Code to unschedule the script
+        if (Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue) {
+            Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
+        }
     }
 }
 
-Write-Host "SCCM servers have been blocked in the HOSTS file."
+function BlockSCCM ($undo) {
+    $SCCMClientUninstall = "C:\Windows\ccmsetup\ccmsetup.exe"
+    $hostsPath = "$env:windir\System32\drivers\etc\hosts"
+    $hostsContent = Get-Content -Path $hostsPath
+    $hostsEntries = $SCCMServers | ForEach-Object {
+        "127.0.0.1 $_"
+        }
+        if (-not $undo) {
+    if (Test-Path $SCCMClientUninstall) {
+        Invoke-Expression "$SCCMClientUninstall /uninstall /quiet /norestart"
+        Write-Host "SCCM Client has been uninstalled silently."
+    } else {
+        Write-Host "SCCM Client is not installed or the path is incorrect."
+    }
 
-# Reset Group Policy Objects
-try {
-    if (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft") { Remove-Item "HKLM:\SOFTWARE\Policies\Microsoft" -Recurse -Force }
-    if (Test-Path "$env:SystemRoot\System32\GroupPolicyUsers") { Remove-Item "$env:SystemRoot\System32\GroupPolicyUsers" -Recurse -Force }
-    if (Test-Path "$env:SystemRoot\System32\GroupPolicy") { Remove-Item "$env:SystemRoot\System32\GroupPolicy" -Recurse -Force }
-    if (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies") { Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" -Recurse -Force }
-    if (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects") { Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects" -Recurse -Force }
-    if (Test-Path "HKCU:\SOFTWARE\Policies\Microsoft") { Remove-Item "HKCU:\SOFTWARE\Policies\Microsoft" -Recurse -Force }
-} catch {
-    Write-Host "Error resetting Group Policy Objects: $_"
-}
-
-# Set registry key for Windows Update
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
-$registryKey = "DoNotConnectToWindowsUpdateInternetLocations"
-
-If (-Not (Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force
-}
-Set-ItemProperty -Path $registryPath -Name $registryKey -Value 0
-
-# Output to verify Windows Update setting change
-Write-Host "DoNotConnectToWindowsUpdateInternetLocations: $((Get-ItemProperty -Path $registryPath).$registryKey)"
-
-# Schedule this script to run every 15 minutes and on startup
-$ScriptPath = "c:\temp\enable-wu.ps1"
-$TaskName = "RunWUEnableScript"
-$TaskDescription = "Runs a PowerShell script every 15 minutes to ensure Windows Update is enabled and SCCM is blocked."
-$TaskFolderPath = "\"
-
-# Check if the task already exists
-if (-not (Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue)) {
-    $nextMinute = (Get-Date).AddMinutes(1)
-    $trigger = New-ScheduledTaskTrigger -Once -At $nextMinute -RepetitionInterval (New-TimeSpan -Minutes 15)
-    $action = New-ScheduledTaskAction -Execute "C:\Program Files\PowerShell\7\pwsh.exe" -Argument "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$ScriptPath`""
-    $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-    $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
-    $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings
-    Register-ScheduledTask -TaskName $TaskName -TaskPath "\" -InputObject $task
-
-    Write-Host "Scheduled task created successfully and will run every 15 minutes and on startup."
+    foreach ($entry in $hostsEntries) {
+        $hostsContent = Get-Content -Path $hostsPath
+        if ($hostsContent -notcontains $entry) {
+            Add-Content -Path $hostsPath -Value $entry
+        } else {
+            Write-Output "Entry '$entry' already exists in the HOSTS file. Skipping..."
+        }
+    }
+    Write-Host "SCCM servers have been blocked in the HOSTS file."
 } else {
-    Write-Host "Scheduled task already exists. No action taken."
+    foreach ($entry in $hostsEntries) {
+        if ($hostsContent -contains $entry) {
+            $hostsContent = $hostsContent | Where-Object { $_ -ne $entry }
+        }
+    }
+    if ((Get-Content -Path $hostsPath) -ne $hostsContent) {
+        $hostsContent | Set-Content -Path $hostsPath
+        Write-Host "SCCM servers have been unblocked in the HOSTS file."
+        }
+    }
 }
+function ResetGroupPolicyObjects ($undo){
+    if (-not $undo) {
+    try {
+        if (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft") { Remove-Item "HKLM:\SOFTWARE\Policies\Microsoft" -Recurse -Force }
+        if (Test-Path "$env:SystemRoot\System32\GroupPolicyUsers") { Remove-Item "$env:SystemRoot\System32\GroupPolicyUsers" -Recurse -Force }
+        if (Test-Path "$env:SystemRoot\System32\GroupPolicy") { Remove-Item "$env:SystemRoot\System32\GroupPolicy" -Recurse -Force }
+        if (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies") { Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" -Recurse -Force }
+        if (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects") { Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects" -Recurse -Force }
+        if (Test-Path "HKCU:\SOFTWARE\Policies\Microsoft") { Remove-Item "HKCU:\SOFTWARE\Policies\Microsoft" -Recurse -Force }
+        Write-Host "Group Policy Objects have been reset."
+    } catch {
+        Write-Host "Error resetting Group Policy Objects: $_"
+    }
+    } else {
+        Write-Host "Group Policy Objects will not be reset."
+    }
+}
+function EnableWindowsUpdate ($undo) {
+    if (-not $undo) {
+    $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
 
-# Disable Group Policy Client Service
-Set-Service -Name gpsvc -StartupType Disabled
-Stop-Service -Name gpsvc -Force
-Write-Host "Group Policy Client Service has been disabled and stopped."
+    if (-Not (Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force
+    }
+
+    Set-ItemProperty -Path $registryPath -Name "DoNotConnectToWindowsUpdateInternetLocations" -Value 0
+    Write-Host "Windows Update settings have been configured to connect to Windows Update servers."
+} else {
+    if (Test-Path $registryPath) {
+        Remove-Item -Path $registryPath -Recurse -Force
+        Write-Host "Windows Update settings have been reset."
+    }
+    }
+}
+function DisableGroupPolicyService($undo) {
+    if (-not $undo) {
+    Set-Service -Name gpsvc -StartupType Disabled
+    Stop-Service -Name gpsvc -Force
+    Write-Host "Group Policy Client Service has been disabled and stopped."
+} else {
+    # Code to enable Group Policy Client Service
+    if ((Get-Service -Name gpsvc).StartType -eq 'Disabled') {
+        Set-Service -Name gpsvc -StartupType Automatic
+        Start-Service -Name gpsvc
+        Write-Host "Group Policy Client Service has been enabled and started."
+    }
+}
+}
+function ForceGroupPolicyUpdate($undo) {
+if (-not $undo) {
+    Write-Host "Group Policy update will not be forced."
+} else { 
+    Invoke-Expression "gpupdate /force"
+    Write-Host "Group Policy update has been started in the background."
+}
+}
+    # Main logic
+    AdminCheck
+    ManagePowerShellInstallation
+    ScheduleScript($undo)
+    BlockSCCM($undo)
+    ResetGroupPolicyObjects($undo)
+    EnableWindowsUpdate($undo)
+    DisableGroupPolicyService($undo)
+    ForceGroupPolicyUpdate($undo)
